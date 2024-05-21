85befb27e388e7156235bd497e4a939d924f89cb
Zoey <153301664+SalenskiZo375@users.noreply.github.com>
2024-05-21 10:56:40 -0400

    its not fully it but its good

--- main.js
+++ main.js
@@ -2,3 +2,16 @@
 // Current date
 // JS IF Statements
 
+// const credits= prompt("how many credits have you earned?");
+
+const creditsToGraduate = prompt("how many credits have you earned?");
+const total = 210 - creditsToGraduate;
+
+
+if (creditsToGraduate >= 210) { 
+	alert (`Congrats, you have enough credits to graduate!`);
+} else {
+	alert (`You do not have enough credits :(. you need ${total} more to graduate.`);
+}
+
+
